48e3d4601553582d50b8ad1bfd32504c58ee0a42
Patrick Kilgore <patrick.kilgore@divvydose.com>
2022-07-14 18:45:04 -0500

    fix: test (#13)

--- src/routes/auth/Login.test.tsx
+++ src/routes/auth/Login.test.tsx
@@ -4,6 +4,6 @@ import Login from "./Login";
 describe("<Login />", () => {
   test("to render login text", () => {
     render(<Login />);
-    expect(screen.getByTestId("login-container")).toHaveTextContent("login");
+    expect(screen.getByText(/Google/)).toBeInTheDocument();
   });
 });
